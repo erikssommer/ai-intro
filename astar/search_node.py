112342bf7f430ff9ce98580b2cd246e9b0613f29
Erik Storås Sommer <erikssommer@gmail.com>
2022-09-15 15:32:10 +0200

    Simplyfied node class

--- astar/search_node.py
+++ astar/search_node.py
@@ -1,15 +1,17 @@
 from state import *
 
 class Search_node:
-    def __init__(self, state: State, status, parent, kids) -> None:
-        self.state = state
-        self.g = 0
-        self.h = 0
-        self.status = status
-        self.parent = parent
-        self.kids = kids
-        
+    def __init__(self, state, parent, path_cost, est_total_cost) -> None:
+        self.parent = parent # parent node
+        self.path_cost = path_cost # cost from the starting node to this node
+        self.est_total_cost = est_total_cost # estimated total costs: f(n)=g(n)+h(n)
+        self.state = state # coordinates of the given position (x, y)
     
-    def get_f(self):
-        return self.g + self.h
+    def __eq__(self, other):
+        return self.est_total_cost == other.est_total_cost
     
+    def __gt__(self, other):
+        return self.est_total_cost > other.est_total_cost
+
+    def __lt__(self, other):
+        return self.est_total_cost < other.est_total_cost
