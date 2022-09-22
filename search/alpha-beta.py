f1f165e828361e8431a405480a4e8201a2e064d8
Erik Storås Sommer <erikssommer@gmail.com>
2022-09-22 09:35:17 +0200

    Implemented utility methods

--- search/alpha-beta.py
+++ search/alpha-beta.py
@@ -4,4 +4,30 @@ class AlphaBeta:
         self.root = game_tree.root
         return
     
+
+    def alpha_beta_search(self, node):
+
+
+    def max_value(self, node, alpha, beta):
+
+
+    def min_value(self, node, alpha, beta):
+
+
+    # Utility methods
+
+    # Successor states in a game tree are the child nodes
+    def getSuccessors(self, node):
+        assert node is not None
+        return node.children
     
+    # return true if the node has NO children
+    # return false if the node has children
+    def isTerminal(self, node):
+        assert node is not None
+        return len(node.children) == 0
+    
+    # returns the value of the node
+    def getUtility(self, node):
+        assert node is not None
+        return node.value
