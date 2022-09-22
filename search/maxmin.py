04e9c6bb9beab0a6cb6131da5dba311032631682
Erik Storås Sommer <erikssommer@gmail.com>
2022-09-22 09:20:44 +0200

    Fixed method reference in recursion

--- search/maxmin.py
+++ search/maxmin.py
@@ -1,20 +1,19 @@
 # A simple Python3 program to find
 # maximum score that
 # maximizing player can get
-
 import math
 
-def maxmin(curDepth, nodeIndex, maxTurn, scores, targetDepth):
+def maxmin(curDepth, nodeIndex, isMax, scores, targetDepth):
     # best care : targetDepth reached
     if curDepth == targetDepth:
         return scores[nodeIndex]
 
-    if maxTurn:
-        return max(minmax(curDepth + 1, nodeIndex * 2, False, scores, targetDepth),
-            minmax(curDepth + 1, nodeIndex * 2 + 1, False, scores, targetDepth))
+    if isMax:
+        return max(maxmin(curDepth + 1, nodeIndex * 2, False, scores, targetDepth),
+            maxmin(curDepth + 1, nodeIndex * 2 + 1, False, scores, targetDepth))
     else:
-        return min(minmax(curDepth + 1, nodeIndex * 2, True, scores, targetDepth),
-            minmax(curDepth + 1, nodeIndex * 2 + 1, True, scores, targetDepth))
+        return min(maxmin(curDepth + 1, nodeIndex * 2, True, scores, targetDepth),
+            maxmin(curDepth + 1, nodeIndex * 2 + 1, True, scores, targetDepth))
     
 
 # Driver code
