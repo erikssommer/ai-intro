96c4b7454a7474e0d825acc303415d9757dd67d1
Erik Storås Sommer <erikssommer@gmail.com>
2022-09-22 15:01:55 +0200

    Starting script done

--- astar/main.py
+++ astar/main.py
@@ -2,12 +2,15 @@ import Map
 import astar
 import sys
 
+from search_node import Search_node
+
 def find_path(node, map_obj):
     current_node = node
+    goalNode = Search_node(map_obj.get_goal_pos())
 
     while current_node.parent is not None:
-        if current_node != map_obj.get_goal_pos():
-            map_obj.set_cell_value(current_node.position, 5)
+        if current_node.state != goalNode.state:
+            map_obj.set_cell_value(current_node.state, 5)
 
         current_node = current_node.parent
     
@@ -18,11 +21,9 @@ def main(task_number):
     map_obj = Map.Map_Obj(task=task_number)
     run = astar.best_first_search(map_obj)
 
-    try:
-        find_path(run, map_obj)
-    except AttributeError:
-        print("An error accoured while finding path")
+    find_path(run, map_obj)
 
+    
 try:
     input = sys.argv[1]
 
@@ -31,9 +32,13 @@ try:
     except:
         print("You have to pass a variable containing task number, try again!")
 
-    if input <= 0 or input > 3:
-        print("Task number must be between 1 and 3")
+    if input <= 0 or input > 4:
+        print("Task number must be between 1 and 4")
     else:
-        main(input)
+        try:
+            main(input)
+        except:
+            print("Could not find path")
+
 except Exception as e:
     print(e)
