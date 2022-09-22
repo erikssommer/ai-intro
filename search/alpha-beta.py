ae3a36ce8a57b6ae90ba14e055432def000ea97b
Erik Storås Sommer <erikssommer@gmail.com>
2022-09-22 10:39:38 +0200

    Implemented alpha-beta search algorithm

--- search/alpha-beta.py
+++ search/alpha-beta.py
@@ -1,3 +1,8 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.children = []
+
 class AlphaBeta:
     def __init__(self, game_tree) -> None:
         self.game_tree = game_tree
@@ -6,13 +11,55 @@ class AlphaBeta:
     
 
     def alpha_beta_search(self, node):
+        infinity = float('inf')
+        best_val = -infinity
+        beta = infinity
 
+        successors = self.getSuccessors(node)
+        best_state = None
+        for state in successors:
+            value = self.min_value(state, best_val, beta)
+            if value > best_val:
+                best_val = value
+                best_state = state
+        print("AlphaBeta: Utility Value of Root Node: = " + str(best_val))
+        print("AlphaBeta: Best State is: " + best_state.Name)
+        return best_state
 
     def max_value(self, node, alpha, beta):
+        print("AlphaBeta --> Max: Visited node :: " + node.Name)
+        if self.isTerminal(node):
+            return self.getUtility(node)
+        
+        infinity = float('inf')
+        value = -infinity
+
+        succressors = self.getSuccessors(node)
+
+        for state in succressors:
+            value = max(value, self.min_value(state, alpha, beta))
+            if value >= beta:
+                return value
+            alpha = max(alpha, value)
+        return value
 
 
     def min_value(self, node, alpha, beta):
+        print("AlphaBeta --> Min: Visited node :: " + node.Name)
+        if self.isTerminal(node):
+            return self.getUtility(node)
+        
+        infinity = float('inf')
+        value = -infinity
 
+        successors = self.getSuccessors(node)
+        for state in successors:
+            value = min(value, self.max_value(state, alpha, beta))
+            if value <= alpha:
+                return value
+            beta = min(beta, value)
+        
+        return value
 
     # Utility methods
 
@@ -31,3 +78,7 @@ class AlphaBeta:
     def getUtility(self, node):
         assert node is not None
         return node.value
+
+def main():
+    game_tree = None
+    search = AlphaBeta(game_tree)
