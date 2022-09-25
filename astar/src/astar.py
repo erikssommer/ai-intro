f3126b14df112a02d77eef498241bfffbaca63dd
Erik Storås Sommer <erikssommer@gmail.com>
2022-09-25 17:16:48 +0200

    Optimized algorithms

--- astar/src/astar.py
+++ astar/src/astar.py
@@ -1,60 +1,60 @@
+from map import Map_Obj
 from node import Node
 
 
-def best_first_search(map_obj):
-    open = []
-    closed = []
+def best_first_search(map_obj: Map_Obj):
+    open: list[Node] = []
+    closed: list[Node] = []
 
-    root_node = Node(map_obj.get_start_pos())
+    start_node = Node(map_obj.get_start_pos())
     goal_node = Node(map_obj.get_goal_pos())
 
-    root_node.g = 0
-    root_node.h = manhattan_distance(root_node, goal_node)
-    root_node.calculate_f()
+    start_node.g = 0
+    start_node.h = manhattan_distance(start_node, goal_node)
+    start_node.calculate_f()
 
-    open.append(root_node)
+    open.append(start_node)
 
     # Agenda loop
     while open:
-
         if len(open) == 0:
-            print("Failed")
             return None
 
-        current_node = open.pop()
+        current_node: Node = open.pop()
+
         closed.append(current_node)
 
-        if current_node.__eq__(goal_node):
-            return current_node
+        if current_node == goal_node:
+            return current_node  # Solution
 
-        path = current_node.generate_all_successors(map_obj)
+        successors = current_node.generate_all_successors(map_obj)
 
-        for node in path:
-            node_state = node_open_closed_check(node, open, closed)
-
-            if node_state not in open and node_state not in closed:
-                attach_and_eval(node_state, current_node, map_obj)
-                open.append(node_state)
+        for successor in successors:
+            node = uniqueness_check(successor, open, closed)
+            current_node.children.append(successor)
+            if node not in open and node not in closed:
+                attach_and_eval(node, current_node, map_obj)
+                open.append(node)
                 open.sort(key=lambda node: node.f, reverse=True)
-            elif (current_node.g + arc_cost(node_state, map_obj)) < node_state.g:
-                attach_and_eval(node_state, current_node, map_obj)
-                if node_state in closed:
-                    propagate_path_improvements(node_state, map_obj)
+            elif (current_node.g + arc_cost(node, map_obj)) < node.g:
+                attach_and_eval(node, current_node, map_obj)
+                if node in closed:
+                    propagate_path_improvements(node, map_obj)
 
+# Returns node if previously created, and successor if not
+def uniqueness_check(successor: Node, open: list[Node], closed: list[Node]) -> Node:
+    for node in open:
+        if node == successor:
+            return node
 
-def node_open_closed_check(node, open, closed):
-    for i in open:
-        if i.state == node.state:
-            return i
+    for node in closed:
+        if node == successor:
+            return node
 
-    for i in closed:
-        if i.state == node.state:
-            return i
+    return successor
 
-    return node
-
-
-def attach_and_eval(child, parent, map_obj):
+# Attaches a child node to a node that is considered its best parent so far
+def attach_and_eval(child: Node, parent: Node, map_obj: Map_Obj) -> None:
     child.parent = parent
     child.g = parent.g + arc_cost(child, map_obj)
     child.h = manhattan_distance(
@@ -63,15 +63,16 @@ def attach_and_eval(child, parent, map_obj):
 
 
 # Returns the Manhattan distance between the two nodes
-def manhattan_distance(curr_node, goal_node):
+def manhattan_distance(curr_node: Node, goal_node: Node):
     return abs(curr_node.state[0] - goal_node.state[0]) + abs(curr_node.state[1] - goal_node.state[1])
 
 
-def arc_cost(child, map_obj):
+def arc_cost(child: Node, map_obj: Map_Obj) -> int:
     return map_obj.get_cell_value(child.state)
 
 
-def propagate_path_improvements(parent, map_obj):
+def propagate_path_improvements(parent: Node, map_obj: Map_Obj) -> None:
+    child: Node
     for child in parent.children:
         if parent.g + arc_cost(child, map_obj) < child.g:
             child.parent = parent
