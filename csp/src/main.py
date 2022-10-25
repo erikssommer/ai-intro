b61025dda8241d8e2e38f814e35fabd32a35aa3b
Erik Storås Sommer <erikssommer@gmail.com>
2022-10-25 15:46:21 +0200

    printing board level

--- csp/src/main.py
+++ csp/src/main.py
@@ -4,5 +4,7 @@ import sys
 level = sys.argv[1]
 csp = create_sudoku_csp(f'{level}.txt')
 print_sudoku_solution(csp.backtracking_search())
-print(f"Number of times backtrack method was called: {csp.backtrack_count}")
-print(f"Number of times backtrack method failed: {csp.failure_count}")
+
+print(f'Board level: {level}')
+print(f'Backtrack method called: {csp.backtrack_count}')
+print(f'Backtrack method failed: {csp.failure_count}')
